5ef32b7b16e1dbb6cfb653448b08f15b8f8dffd7
Your Name <bshx2024@gmail.com>
2026-02-16 17:27:43 +0800

    chore: add vercel.json and revert config to ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
-console.log("React Router Config Loaded");
+import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-    ssr: true,
+    // ssr: true,
     presets: [vercelPreset()],
-};
+} satisfies Config;
