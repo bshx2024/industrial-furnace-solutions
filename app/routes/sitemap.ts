952a37997a138e8eca023acd29d0f2609f236ad2
Your Name <bshx2024@gmail.com>
2026-02-16 21:14:37 +0800

    fix: convert sitemap to a pure resource route returning XML

--- app/routes/sitemap.ts
+++ app/routes/sitemap.ts
@@ -1,44 +1,67 @@
 import type { LoaderFunctionArgs } from "react-router";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const baseUrl = "https://www.ecoreheating.com";
-
-  const pages = [
-    { loc: "", lastmod: new Date().toISOString().split('T')[0] },
-    { loc: "/solutions", lastmod: new Date().toISOString().split('T')[0] },
-    { loc: "/hero-cases", lastmod: new Date().toISOString().split('T')[0] },
-    { loc: "/case-studies", lastmod: new Date().toISOString().split('T')[0] },
-    { loc: "/about", lastmod: new Date().toISOString().split('T')[0] },
-  ];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  ${pages.map(page => `
   <url>
-    <loc>${baseUrl}${page.loc}/</loc>
-    <lastmod>${page.lastmod}</lastmod>
-    <xhtml:link rel="alternate" hreflang="en" href="${baseUrl}${page.loc}/" />
-    <xhtml:link rel="alternate" hreflang="vi" href="${baseUrl}/vi${page.loc}/" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="${baseUrl}${page.loc}/" />
+    <loc>https://www.ecoreheating.com/</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.ecoreheating.com/" />
+    <priority>1.0</priority>
+    <changefreq>weekly</changefreq>
   </url>
   <url>
-    <loc>${baseUrl}/vi${page.loc}/</loc>
-    <lastmod>${page.lastmod}</lastmod>
-    <xhtml:link rel="alternate" hreflang="en" href="${baseUrl}${page.loc}/" />
-    <xhtml:link rel="alternate" hreflang="vi" href="${baseUrl}/vi${page.loc}/" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="${baseUrl}${page.loc}/" />
-  </url>`).join('')}
+    <loc>https://www.ecoreheating.com/vi/</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/" />
+    <priority>1.0</priority>
+    <changefreq>weekly</changefreq>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/solutions</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/solutions" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/solutions" />
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/vi/solutions</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/solutions" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/solutions" />
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/hero-cases</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/hero-cases" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/hero-cases" />
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/vi/hero-cases</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/hero-cases" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/hero-cases" />
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/about</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/about" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/about" />
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.ecoreheating.com/vi/about</loc>
+    <xhtml:link rel="alternate" hreflang="en" href="https://www.ecoreheating.com/about" />
+    <xhtml:link rel="alternate" hreflang="vi" href="https://www.ecoreheating.com/vi/about" />
+    <priority>0.7</priority>
+  </url>
 </urlset>`;
 
-  return new Response(sitemap, {
+  return new Response(xmlContent, {
     status: 200,
     headers: {
       "Content-Type": "application/xml; charset=utf-8",
       "X-Content-Type-Options": "nosniff",
     },
   });
-}
-export default function Sitemap() {
-  return null;
 }
